9c1601b53686f71ac4d421e966d146a28076ebdb
Hoang Tien Dat <hoangtiendat.thidaihoc@gmail.com>
2022-11-19 10:35:58 +0700

    Initial Commit

---
+++ SwiftExample/SwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExampleApp.swift
+//  SwiftExample
+//
+//  Created by Hoang Tien Dat on 19/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
